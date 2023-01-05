5f4ca06e43649dbf54bef78dcc374a70a30f209c
actuallyvedika <119405050+actuallyvedika@users.noreply.github.com>
2023-01-05 10:16:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const server = jsonserver.create();
 const router = jsonserver.router("db.json");
 // const middleware = jsonserver.defaults();
 const PORT = 3000;
+const cors = require("cors)
 
 // server.use(middleware);
 server.use(router);
+server.use(cors())
 
 server.listen(PORT);
